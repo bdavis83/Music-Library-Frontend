f0902ac3a9ffdf1de7c4d262191470948b29f49d
bdavis83 <bdavis83@gmail.com>
2023-03-02 13:01:56 -0500

    Wrote displayNav function for the NavigationBar component

---
+++ music-library-frontend/src/Components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,11 @@
+
+
+const displayNav = (props) => {
+    return ( 
+        <nav className="navbar">
+            <h3 className="navheader">Music Play List</h3>
+        </nav>
+     );
+}
+ 
+export default displayNav;
