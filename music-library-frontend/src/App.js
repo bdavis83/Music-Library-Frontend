8409d96638198b08915eae03d7a1798826f52a4c
bdavis83 <bdavis83@gmail.com>
2023-03-02 12:33:17 -0500

    Added logic in the App function to get all songs

--- music-library-frontend/src/App.js
+++ music-library-frontend/src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [songs, setSongs] = useState([])
+
+  useEffect (()=>{
+    getAllSongs();
+  }, []);
+
+  async function getAllSongs(){
+    const response = await axios.get('http://127.0.0.0:8000/api/music/');
+    /*console.log (response.data)*/
+    setSongs (response.data)
+  }
   return (
     <div className="App">
+      <button onClick={()=> getAllSongs()}>Get All Songs</button>
   
     </div>
   );
