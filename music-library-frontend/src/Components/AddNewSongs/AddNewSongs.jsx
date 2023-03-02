d4793f9a0c86a46e3891e1e319414fd1f4a77585
bdavis83 <bdavis83@gmail.com>
2023-03-02 14:01:05 -0500

    wrote logic for return statment for title, artist, albume, release date, genre

--- music-library-frontend/src/Components/AddNewSongs/AddNewSongs.jsx
+++ music-library-frontend/src/Components/AddNewSongs/AddNewSongs.jsx
@@ -3,14 +3,19 @@ import React, { useState } from 'react';
 
 const  AddNewSongs = (props) => {
 
-    const [song, setSongs] = useState ()
+    const [title, setTitle] = useState ('');
+    const [artist, setArtist] = useState ('');
+    const [album, setAlbum] = useState ('');
+    const [release_date, setReleaseDate] = useState ('');
+    const [genre, setGenre] = useState ('');
+
 
     function handleSubmit (event){
         event.preventDefault();
         let newEntry = {
             title: title,
             artist: artist,
-            albume: albume,
+            album: album,
             release_date: release_date,
             genre: genre
         };
@@ -19,8 +24,26 @@ const  AddNewSongs = (props) => {
     }
     return ( 
         <form onSubmit={handleSubmit} className='form-grid'>
-
-            
+            <div className='form-group'>
+                <label>Title</label>
+                <input type = 'string' className='form-control' value ={title} onChange={(event)=>setTitle(event.target.value)}></input>
+            </div>
+            <div className='form-group'>
+                <label>Artist</label>
+                <input type = 'string' className='form-control' value ={artist} onChange={(event)=>setArtist(event.target.value)}></input>
+            </div>
+            <div className='form-group'>
+                <label>Album</label>
+                <input type = 'string' className='form-control' value ={album} onChange={(event)=>setAlbum(event.target.value)}></input>
+            </div>
+            <div className='form-group'>
+                <label>Release Date</label>
+                <input type = 'string' className='form-control' value ={release_date} onChange={(event)=>setReleaseDate(event.target.value)}></input>
+            </div>
+            <div className='form-group'>
+                <label>Genre</label>
+                <input type = 'string' className='form-control' value ={genre} onChange={(event)=>setGenre(event.target.value)}></input>
+            </div>
         </form>
      );
 }
